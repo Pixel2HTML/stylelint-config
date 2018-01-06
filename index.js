aeb6407a663bd59df3bcca019538edeef59e9fda
Miguel Palau <mpalauzarza@gmail.com>
2018-01-05 23:31:30 -0600

    add css modules support

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ module.exports = {
       }
     ],
     // Don't trigger error on empty or yet to be filled files
-    'no-empty-source': null
+    'no-empty-source': null,
+    'selector-pseudo-class-no-unknown': [ true, {
+      // CSS Modules FTW
+      'ignorePseudoClasses': [
+        'export',
+        'import',
+        'global',
+        'local'
+      ]
+    }]
   }
 }
