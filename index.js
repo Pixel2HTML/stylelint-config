fec601ecc85a9b9963a8ad6a380ca3622f9769c8
Miguel Palau <mpalauzarza@gmail.com>
2018-02-22 11:42:46 -0600

    removing unsupported browser features

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-const browsers = require('@pixel2html/browserlist')
-
 module.exports = {
   extends: 'stylelint-config-recommended-scss',
   plugins: [
     'stylelint-no-unsupported-browser-features'
   ],
   rules: {
-    // Responsible development
-    // Always use autoprefixer :)
-    'plugin/no-unsupported-browser-features': [
-      true,
-      {
-        'browsers': browsers,
-        'ignore': ['rem'],
-        'severity': 'warning'
-      }
-    ],
     // Don't trigger error on empty or yet to be filled files
     'no-empty-source': null,
     'selector-pseudo-class-no-unknown': [ true, {
