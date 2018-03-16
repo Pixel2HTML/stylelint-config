bdc41133404c49d03fa26c84bebec6256d8b12cc
Miguel Palau <mpalauzarza@gmail.com>
2018-03-16 14:05:56 -0600

    modules first

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
+    'stylelint-config-css-modules',
     'stylelint-config-recommended-scss',
-    'stylelint-config-css-modules'
   ],
   rules: {
     // Don't trigger error on empty or yet to be filled files
