1fc7a161fd639c7666d974d64ccdb6002b260c72
Miguel Palau <mpalauzarza@gmail.com>
2018-03-16 14:10:38 -0600

    removing modules and making it explicit

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 module.exports = {
-  extends: [
-    'stylelint-config-css-modules',
-    'stylelint-config-recommended-scss',
-  ],
+  extends: 'stylelint-config-recommended-scss',
   rules: {
     // Don't trigger error on empty or yet to be filled files
     'no-empty-source': null,
+    // css modules situation my ninja
+    'selector-pseudo-class-no-unknown': [ true, {
+      ignorePseudoClasses: [
+        'export',
+        'import',
+        'global',
+        'local',
+      ],
+    }],
+    'property-no-unknown': [ true, {
+      ignoreProperties: [
+        'composes',
+        'compose-with',
+      ],
+    }],
+    'at-rule-no-unknown': [ true, {
+      ignoreAtRules: [
+        'value',
+      ],
+    }],
   }
 }
