aa60a6c348214431d4ffcbe76f35d4783b5fecdf
Miguel Palau <mpalauzarza@gmail.com>
2018-03-16 13:40:02 -0600

    extending css modules config as well

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 module.exports = {
-  extends: 'stylelint-config-recommended-scss',
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-css-modules'
+  ],
   rules: {
     // Don't trigger error on empty or yet to be filled files
     'no-empty-source': null,
-    'selector-pseudo-class-no-unknown': [ true, {
-      // CSS Modules FTW
-      'ignorePseudoClasses': [
-        'export',
-        'import',
-        'global',
-        'local',
-        'composes'
-      ]
-    }]
   }
 }
